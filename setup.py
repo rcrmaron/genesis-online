eee986c44d0a128f0a3dcdf74db2ce33f5b09a7b
rcrmaron <rcrmaron@gmail.com>
2023-07-24 20:23:36 +0200

    Add package setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='genesisonline',
+    version='0.1.0',
+    author='Roman Maron',
+    description='Inofficial Python API for the Genesis Online API of the German Federal Statistical Office',
+    packages=find_packages(),
+    install_requires=[],
+)
