7ea50b8c57416858438ac667ba1459f293533912
rcrmaron <rcrmaron@gmail.com>
2023-07-31 21:14:15 +0200

    Add try except block for request

--- genesisonline/services/base.py
+++ genesisonline/services/base.py
@@ -21,33 +21,23 @@ class BaseService(ABC):
         url = self.BASE_URL + endpoint
         params = self._session.params | kwargs
 
-        response = self._session.get(url, params=params)
-        print(f"Requesting from {response.url}")
-        return response.json()
+        try:
+            response = self._session.get(url, params=params)
+            response.raise_for_status()  # Raise an exception for non-2xx status codes
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            # Handle network-related errors
+            print(f"Network error: {e}")
+        except ValueError as e:
+            # Handle JSON parsing errors
+            print(f"JSON parsing error: {e}")
+        except Exception as e:
+            # Handle other unexpected errors
+            print(f"Unexpected error: {e}")
+
+        return None
 
     @abstractmethod
     def endpoints(self) -> list:
         """Return a list of all available endpoints for the service."""
         pass
-
-    def __request(self, url):
-        # print(url)
-        try:
-            response = self.session.get(url, timeout=self.request_timeout)
-        except requests.exceptions.RequestException:
-            raise
-
-        try:
-            response.raise_for_status()
-            content = json.loads(response.content.decode("utf-8"))
-            return content
-        except Exception as e:
-            # check if json (with error message) is returned
-            try:
-                content = json.loads(response.content.decode("utf-8"))
-                raise ValueError(content)
-            # if no json
-            except json.decoder.JSONDecodeError:
-                pass
-
-            raise
