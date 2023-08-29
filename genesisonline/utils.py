40d72bb21cfd2f6d888b16b2acee11ef2ed9af48
rcrmaron <rcrmaron@gmail.com>
2023-08-29 20:49:10 +0200

    refactor and black formatting

--- genesisonline/utils.py
+++ genesisonline/utils.py
@@ -1,11 +1,7 @@
-def get_api_params(
-    func_args: dict,
-    exclude: list = list(),
-) -> dict:
+def get_api_params(func_args: dict, exclude: list = list()) -> dict:
     exclude.extend(["self", "__class__"])
-    api_kwargs = func_args.pop("kwargs", dict())
+    api_params = func_args.pop("kwargs", dict())
     for k, v in func_args.items():
         if k not in exclude:
-            api_kwargs[k] = v
-    print(api_kwargs)
-    return api_kwargs
+            api_params[k] = v
+    return api_params
