f92e39c3ce27da516037bc5e67bcb35cdb26df76
rcrmaron <rcrmaron@gmail.com>
2023-08-29 20:49:58 +0200

    define library exceptions

---
+++ genesisonline/exceptions.py
@@ -0,0 +1,44 @@
+class UnexpectedParameterWarning(UserWarning):
+    pass
+
+
+class GenesisOnlineError(Exception):
+    """Base exception for GenesisOnline"""
+
+    pass
+
+
+class HTTPError(GenesisOnlineError):
+    """Exception for HTTP errors"""
+
+    pass
+
+
+class ConnectionError(GenesisOnlineError):
+    """Exception for connection errors"""
+
+    pass
+
+
+class TimeoutError(GenesisOnlineError):
+    """Exception for timeout errors"""
+
+    pass
+
+
+class RequestError(GenesisOnlineError):
+    """Exception for request errors"""
+
+    pass
+
+
+class UnexpectedContentError(GenesisOnlineError):
+    """Exception for unexpected content type"""
+
+    pass
+
+
+class StandardizationError(GenesisOnlineError):
+    """Exception for"""
+
+    pass
