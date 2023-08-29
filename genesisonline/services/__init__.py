57333a5559dfd960a5362cd5ce224460511adeef
rcrmaron <rcrmaron@gmail.com>
2023-08-29 20:45:48 +0200

    add remaining services

--- genesisonline/services/__init__.py
+++ genesisonline/services/__init__.py
@@ -1,4 +1,6 @@
+from .base import BaseService
 from .test import TestService
 from .find import FindService
 from .catalogue import CatalogueService
 from .data import DataService
+from .metadata import MetadataService
